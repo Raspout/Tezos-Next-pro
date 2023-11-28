47d364168f94225bfaeaabfb7fdbfd3357d7dbed
Alexis <alexis.carella@feel-mining.com>
2023-11-28 15:17:53 +0100

    changes

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import grequests
 import json
+import sys
 
 API="https://api.tzkt.io"
 
-if __name__ == "__main__":
-    
+
+def get_API():
     rs = []
     rs.append(grequests.get(API+"/v1/protocols/current"))
-    rs.append(grequests.get(API+"/v1/blocks/count"))
+    if("--show-current" in sys.argv):
+        rs.append(grequests.get(API+"/v1/blocks/count"))
+    return grequests.map(rs)
 
-    responses = grequests.map(rs)
-
+def display(responses):
     if json.loads(responses[0].content).get("lastLevel") is not None:
-        print("protocol finishes at block : " + str(json.loads(responses[0].content)["lastLevel"]))
+        print("proto finishes at block : " + str(json.loads(responses[0].content)["lastLevel"]))
     else:
         print("No protocol update staged yet")
+        
+    if("--show-current" in sys.argv):
+        print("current block count : " + responses[1].content.decode())
 
-    print("current block count : " + responses[1].content.decode())
+if __name__ == "__main__":
+    
+    display(get_API())
